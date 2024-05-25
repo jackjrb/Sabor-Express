5d93a1b37dc3f340e6a87a18f5405f424f6bcd8c
Jackeline Brito <jackeline.brito@maistodos.com.br>
2024-05-25 12:06:13 -0300

    feat: add user and password program

---
+++ user_password.py
@@ -0,0 +1,10 @@
+user = 'Jackeline'
+password = '1234'
+
+user_input = input('Digite o seu usuário:\n')
+password_input = input('Digite a sua senha:\n')
+
+if user == user_input and password==password_input:
+    print('Login efetuado com sucesso')
+else:
+    print('Usuário ou senha inválido')
