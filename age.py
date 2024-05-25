226ec4fba87a801e6668f0bbc3891c3fafa82650
Jackeline Brito <jackeline.brito@maistodos.com.br>
2024-05-25 12:06:28 -0300

    feat: add age program

---
+++ age.py
@@ -0,0 +1,8 @@
+age = int(input('Digite sua idade:\n'))
+
+if age <= 12 :
+    print('Você é uma criança')
+elif 13 <= age <=18 :
+    print('Você é um adolescente')
+else:
+    print('Você é um adulto')
