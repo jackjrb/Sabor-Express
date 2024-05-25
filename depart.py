53fbf1fc8332b4e381ecb39c52cfcfebe4c05445
Jackeline Brito <jackeline.brito@maistodos.com.br>
2024-05-25 12:04:45 -0300

    feat: add department program

---
+++ depart.py
@@ -0,0 +1,3 @@
+departamento = input('Digite o nome do departamento: ')
+responsavel = input('Digite o nome do responsável: ')
+print('O departamento de ' + departamento + 'é liderádo por ' + responsavel )
