c84a7e56d317ff62d6392d4902e639c93f76a106
Jackeline Brito <jackeline.brito@maistodos.com.br>
2024-05-25 12:18:47 -0300

    feat: start menu

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+
+def show_program_name():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+
+def show_options():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finish_app():
+    os.system('clear')
+    #os.system('cli') windowns
+    print('Finalizando app\n')
+
+def choice_option():
+    selected_option = int(input('Escolha uma opção:'))
+    print(f'Você escolheu {selected_option}')
+    #selected_option_number = int(selected_option)
+
+
+    if selected_option == 1:
+        restaurant = input('Cadastrar restaurante')
+    elif selected_option == 2:
+        print('Listar restaurante')
+    elif selected_option == 3:
+        print('Ativar restaurante')
+    else:
+        finish_app()
+
+def main():
+    show_program_name()
+    show_options()
+    choice_option()
+
+if __name__ == '__main__':
+    main()
